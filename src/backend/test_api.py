81e5f9720547445350341e66189ec6ff8ce61f73
hosseinhj1380 <hosseinjavaditj@gmail.com>
2024-01-06 15:09:45 +0330

    test api and chatbot

--- src/backend/test_api.py
+++ src/backend/test_api.py
@@ -13,15 +13,21 @@ from IPython.display import display
 from IPython.display import Markdown
 
 # Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
-GOOGLE_API_KEY=os.environ.get("API_KEY")
-
-genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-pro')
+# GOOGLE_API_KEY=os.environ.get("AIzaSyANjL6qLDhj75XCQ6GRyEkVm527QwzljzQ")
 
 def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-response = model.generate_content("What is the meaning of life?")
+def sart():
 
-to_markdown(response.text)
+  genai.configure(api_key="AIzaSyANjL6qLDhj75XCQ6GRyEkVm527QwzljzQ")
+  model = genai.GenerativeModel('gemini-pro')
+  chat = model.start_chat(history=[])
+  response = chat.send_message("what is my name ?")
+  print(response.text)
+  
+  
+
+
+sart()
